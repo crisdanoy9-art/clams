45787c998ca5f23761c29cc464a8094f501f9970
danoy <crisdanoy9@gmail.com>
2026-04-26 13:51:22 +0330

    Initial commit: set up for reusable fullstack

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-center text-blue-500 underline">
+  Tailwind is Working!
+</h1>
+    </>
+  )
+}
+
+export default App
